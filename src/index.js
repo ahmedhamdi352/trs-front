b3e48bd830e23195858091c97c30b8e791271105
ahmed hamdi <ahmed.hamdi@barqsystems.com>
2022-09-03 09:48:07 +0200

    remove langgg

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import DashApp from './dashApp';
 import * as serviceWorker from './serviceWorker';
-import './i18n'
 import 'antd/dist/antd.min.css';
 
 ReactDOM.render(<DashApp />, document.getElementById('root'));
